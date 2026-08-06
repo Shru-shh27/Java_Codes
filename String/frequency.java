4089e34fcc85098a090bd5e1370f4d06f5f6a8aa
Shruti Shreya <shruti.shreya27@gmail.com>
2026-08-06 20:03:25 +0530

    string manipulation

---
+++ String/frequency.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.util.*;
+
+public class frequency {
+    String s1 = "Hello";System.out.println(s1);
+
+    // frequency of each elements
+    String s2 = "Hello";System.out.println(s2);
+    // Calculate frequency of each character in s2
+    int[] frequency = new int[256];for(
+    int i = 0;i<s2.length();i++)
+    {
+        frequency[s2.charAt(i)]++;
+    }
+    // Print frequency of each character
+    for(
+    int i = 0;i<frequency.length;i++)
+    {
+        if (frequency[i] > 0) {
+            System.out.println((char) i + ": " + frequency[i]);
+        }
+    }
+}
